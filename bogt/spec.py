481d7e41f9e08b8dbddd8c06cdd7ff69c0cb3491
Steve Baker <sbaker@redhat.com>
2016-03-25 11:10:05 +1300

    update spec, move some lookup logic to spec.py

--- bogt/spec.py
+++ bogt/spec.py
@@ -6,11 +6,14 @@ import os
 _tables = None
 
 
-def table(table):
+def table(table_name):
     global _tables
     if _tables is None:
         _load_tables()
-    return _tables[table]
+    try:
+        return _tables[table_name]
+    except KeyError:
+        raise ValueError('Unknown table: %s' % (table_name))
 
 
 def patch():
@@ -23,6 +26,24 @@ def system():
 
 def midi():
     return table('MIDI')
+
+
+def table_name(self, block):
+    if block == 0x0:
+        return 'SYSTEM'
+    if block == 0x2:
+        return 'MIDI'
+    if block >= 0x1000:
+        return 'PATCH'
+    raise ValueError('Unknown address block: %02x', block)
+
+
+def table_entry(self, table_name, table_key):
+    t = table(table_name)
+    try:
+        return t[table_key]
+    except KeyError:
+        raise ValueError('Unknown key in  %s: %s' % (table_name, table_key))
 
 
 def _load_tables():
