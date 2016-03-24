b01d26170e7d6f962b042015430b684bbe6e63a5
Steve Baker <sbaker@redhat.com>
2016-03-25 08:55:25 +1300

    prep for sysex split, implement checksum

--- bogt/cmd/parse.py
+++ bogt/cmd/parse.py
@@ -4,6 +4,11 @@ import struct
 from cliff import command
 import mido
 
+from bogt import spec
+
+
+log = logging.getLogger(__name__)
+
 
 class ParseData(command.Command):
     '''Parse a MIDI file and print patch values'''
@@ -17,60 +22,94 @@ class ParseData(command.Command):
 
     def take_action(self, parsed_args):
         mid = mido.MidiFile(parsed_args.filename)
+        parsed = []
         for msg in mid:
-            if msg.type == 'sysex':
-                ps = ParsedSysex(msg)
-                print(ps)
+            if msg.type != 'sysex':
+                continue
+            parsed.append(ParsedSysex(msg.data))
+
+        # prev_ps = None
+        # parsed_split = []
+        for ps in parsed:
+            # print('%s == %s' % (ps.checksum, ps.calculate_checksum()))
+            print('%s' % ps)
+            # parsed_split.extend(ps.split(prev_ps))
+            # prev_ps = ps
 
 
 class ParsedSysex(object):
 
-    def __init__(self, msg):
-        self.msg = msg
-        if msg.type != 'sysex':
-            raise ValueError('Not a sysex message')
+    def __init__(self, data):
+        self.data = data
 
     @property
     def body(self):
-        return self.msg.data[10:-1]
-
-    @property
-    def data(self):
-        return self.msg.data
+        return self.data[10:-1]
 
     @property
     def checksum(self):
-        return self.msg.data[-1:][0]
+        return self.data[-1:][0]
+
+    def calculate_checksum(self):
+        return 128 - (sum(self.data[6:-1]) % 128)
+
+    @property
+    def size(self):
+        entry = self.table_entry()
+        return entry['size']
 
     @property
     def address_block(self):
         return struct.unpack(
             '>H', struct.pack(
-                'BB', *self.msg.data[6:8]))[0]
+                'BB', *self.data[6:8]))[0]
 
     @property
     def address(self):
         return struct.unpack(
+            '>I', struct.pack(
+                'BBBB', *self.data[6:10]))[0]
+
+    @property
+    def table_key(self):
+        return struct.unpack(
             '>H', struct.pack(
-                'BB', *self.msg.data[8:10]))[0]
+                'BB', *self.data[8:10]))[0]
 
     def is_send(self):
-        return self.msg.data[5] == 0x12
+        return self.data[5] == 0x12
 
     def is_receive(self):
-        return self.msg.data[5] == 0x11
+        return self.data[5] == 0x11
+
+    def table_entry(self):
+        table = spec.table(self.table_name)
+        return table[self.table_key]
 
     def location_label(self):
-        return '?'
+        try:
+            entry = self.table_entry()
+        except KeyError:
+            log.warn('%s not in %s' % (self.table_key, self.table_name))
+            return self.table_key
+        parameter = entry['parameter']
+        return ': '.join(parameter)
 
     @property
     def table_name(self):
         block = self.address_block
-        if block < 0x0020:
+        if block == 0x0:
             return 'SYSTEM'
-        if block < 0x1000:
+        if block == 0x2:
             return 'MIDI'
         return 'PATCH'
+
+    def split(self, prev_ps):
+        try:
+            entry = self.table_entry()
+        except KeyError:
+            print('TODO handle prev_ps')
+        print(entry)
 
     def __str__(self):
         sr = '????'
@@ -81,6 +120,7 @@ class ParsedSysex(object):
 
         # hexdata = ['%02x' % d for d in self.data]
         # strhexdata = ', '.join(hexdata)
+        print(self.location_label())
         return '%s %s %s %s: %s' % (
             sr,
             self.table_name,
