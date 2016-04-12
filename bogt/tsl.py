8a60f03896b465c14eb0bde66936c996ba66c6cf
Steve Baker <sbaker@redhat.com>
2016-04-13 07:55:19 +1200

    patch_to_midi a single patch

--- bogt/tsl.py
+++ bogt/tsl.py
@@ -14,6 +14,29 @@ def load_tsl_from_file(path, conf):
         return LiveSet(json.load(f), conf)
 
 
+def patch_to_midi(conf, patch, preset_name):
+    values = patch['params']
+    upb = spec.table('USER PATCH BLOCK REVERSE')
+    byte_table = spec.table('BYTENUM TO INDEX REVERSE')
+    block = upb[preset_name]
+    device_id = conf.get('device_id', 0x00)
+    pt = spec.patch()
+    for address, param in sorted(pt.items()):
+        param_key = param['parameter_key']
+        value = values[param_key]
+        value = byte_table[value]
+
+        print('\n%s = %s' % (param_key, value))
+        size = param['size']
+        address = param['address']
+        msg_data = parsed_sysex.param_to_send_data(
+            device_id, block, address, size, value)
+        print(parsed_sysex.ParsedSysex(msg_data))
+        io.print_data(msg_data)
+        msg = mido.Message('sysex', data=msg_data)
+        print(msg)
+
+
 class LiveSet(object):
 
     def __init__(self, data, conf):
@@ -26,23 +49,4 @@ class LiveSet(object):
 
     def to_midi(self, patch_key, preset_name):
         patch = self.patches[patch_key]
-        values = patch['params']
-        upb = spec.table('USER PATCH BLOCK REVERSE')
-        byte_table = spec.table('BYTENUM TO INDEX REVERSE')
-        block = upb[preset_name]
-        device_id = self.conf.get('device_id', 0x00)
-        pt = spec.patch()
-        for address, param in sorted(pt.items()):
-            param_key = param['parameter_key']
-            value = values[param_key]
-            value = byte_table[value]
-
-            print('\n%s = %s' % (param_key, value))
-            size = param['size']
-            address = param['address']
-            msg_data = parsed_sysex.param_to_send_data(
-                device_id, block, address, size, value)
-            print(parsed_sysex.ParsedSysex(msg_data))
-            io.print_data(msg_data)
-            msg = mido.Message('sysex', data=msg_data)
-            print(msg)
+        patch_to_midi(self.conf, patch, preset_name)
